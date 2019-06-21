925fa0dc67d85e398b70ae43c7a11182f8b1a293
AJ ONeal <coolaj86@gmail.com>
2019-06-20 21:17:28 -0600

    WIP, nae nae

--- gitver.go
+++ gitver.go
@@ -142,7 +142,7 @@ func semVer(desc string) string {
 }
 
 func gitTimestamp(desc string) (time.Time, error) {
-	args := strings.Split(fmt.Sprintf("git show %s --format='%%cd' --date='format:%%Y-%%m-%%dT%%H:%%M:%%SZ%%z' --no-patch", desc), " ")
+	args := strings.Split(fmt.Sprintf("git show %s --format=%%cd --date=format:%%Y-%%m-%%dT%%H:%%M:%%SZ%%z --no-patch", desc), " ")
 	cmd := exec.Command(args[0], args[1:]...)
 	out, err := cmd.CombinedOutput()
 	fmt.Printf("args:\n%#v\n\n", args)
