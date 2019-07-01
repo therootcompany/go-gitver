a3af8e6c5bb1f4005acfbfa55aae512875290046
AJ ONeal <coolaj86@gmail.com>
2019-07-01 02:33:28 -0600

    v1.1.2

--- version.go
+++ version.go
@@ -3,7 +3,7 @@ package main
 // use recently generated version info as a fallback
 // for when git isn't present (i.e. go run <url>)
 func init() {
-	GitRev = "0b8c2d86df4bfe32ff4534eec84cd56909c398e9"
-	GitVersion = "v1.1.1"
-	GitTimestamp = "2019-06-21T00:18:13-06:00"
+	GitRev = "0921ed1e6007493c886c87ee9a15f2cceecb1f9f"
+	GitVersion = "v1.1.2"
+	GitTimestamp = "2019-07-01T02:32:58-06:00"
 }
